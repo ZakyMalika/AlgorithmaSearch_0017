1d86350c3061f14f566699834f114b5e6b986bf9
ZakyMalika <zakymalika0987@gmail.com>
2024-04-04 16:33:18 +0700

    menggunakan for dan ouputnya  i + 1

--- AlgoSearch/AlgoSearch.cpp
+++ AlgoSearch/AlgoSearch.cpp
@@ -16,4 +16,13 @@ void input()
 		else
 			cout << "\n Array should have minimum 1 and maximum 20 elemnts. \n\n";
 	}
+	// Accept array elements
+	cout << "\n --------------------\n";
+	cout << " Enter Array Elements \n";
+	cout << "\n --------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
 }
